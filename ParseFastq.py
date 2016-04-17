f78092238df4ed11a782c49358ceee8a18085ccc
Enes Kemal Ergin <eneskemalergin@gmail.com>
2016-04-17 17:35:23 -0500

    exact matching and fastq parser added

---
+++ ParseFastq.py
@@ -0,0 +1,16 @@
+def readFastq(filename):
+    sequences = []
+    qualities = []
+    with open(filename) as fh:
+        while True:
+            fh.readline()  # skip name line
+            seq = fh.readline().rstrip()  # read base sequence
+            fh.readline()  # skip placeholder line
+            qual = fh.readline().rstrip() # base quality line
+            if len(seq) == 0:
+                break
+            sequences.append(seq)
+            qualities.append(qual)
+    return sequences, qualities
+
+
