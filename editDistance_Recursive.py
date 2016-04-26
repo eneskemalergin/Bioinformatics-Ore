0b9dab8a7c489fa915b1debfad45603443855e3f
Enes Kemal Ergin <eneskemalergin@gmail.com>
2016-04-25 19:57:00 -0500

    Recursive edit distance

---
+++ editDistance_Recursive.py
@@ -0,0 +1,16 @@
+# Really Slow version of EditDistance
+# Using Recurrsion
+def editDistRecursive(x, y):
+    # This implementation is very slow
+    if len(x) == 0:
+        return len(y)
+    elif len(y) == 0:
+        return len(x)
+    else:
+        distHor = editDistRecursive(x[:-1], y) + 1
+        distVer = editDistRecursive(x, y[:-1]) + 1
+        if x[-1] == y[-1]:
+            distDiag = editDistRecursive(x[:-1], y[:-1])
+        else:
+            distDiag = editDistRecursive(x[:-1], y[:-1]) + 1
+        return min(distHor, distVer, distDiag)
