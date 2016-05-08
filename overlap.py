728cc69d6efd05ea0cbeb02ae40fc2eb3c53d00f
Enes Kemal Ergin <eneskemalergin@gmail.com>
2016-05-07 19:32:29 -0500

    Algorithm to calculate overlap

---
+++ overlap.py
@@ -0,0 +1,14 @@
+def overlap(a, b, min_length=3):
+    """ Return length of longest suffix of 'a' matching
+        a prefix of 'b' that is at least 'min_length'
+        characters long.  If no such overlap exists,
+        return 0. """
+    start = 0  # start all the way at the left
+    while True:
+        start = a.find(b[:min_length], start)  # look for b's suffx in a
+        if start == -1:  # no more occurrences to right
+            return 0
+        # found occurrence; check for full suffix/prefix match
+        if b.startswith(a[start:]):
+            return len(a)-start
+        start += 1  # move just past previous match
