1976c1d6f2eeff8e943b016abbbe39859fe960f7
eneskemalergin <eneskemalergin@gmail.com>
2017-10-05 22:19:44 -0700

    new solution is added

--- rosalind_solutions/014-LCSM.py
+++ rosalind_solutions/014-LCSM.py
@@ -19,10 +19,38 @@ TAGACCA
 ATACA
 
 Sample Output
-AC
+AC (or) TA
 """
+
+from Bio import SeqIO
+
+def longest_motif_match(seqs):
+    # Returns the first longest match
+    seqs = sorted(seqs) # sort the list
+    short_seq = seqs[0] # store the shortest
+    other_seqs = seqs[1:] # exclude shortest from the list
+
+    p = '' # initialize string to keep the motif
+    for i in range(len(short_seq)):
+        for j in range(len(short_seq) , i + len(p), -1):
+            s1 = short_seq[i:j] # Subset the string
+            matched = True
+            for s2 in other_seqs:
+                if s1 not in s2:
+                    matched = False
+                    break
+            if matched:
+                p = s1
+                break
+    return p
+
 def main(data_path):
-    pass
+    lst_seqs = []
+    for sequence in SeqIO.parse(data_path, "fasta"):
+        lst_seqs.append(str(sequence.seq))
+
+    return longest_motif_match(lst_seqs)
+
 
 if __name__ == '__main__':
     run_mode = input('Select a run mode: (T)est, (F)ull ')
@@ -31,7 +59,7 @@ if __name__ == '__main__':
 
     if run_mode in ['T', 't']:
         result = main(test_path)
-        assert result == 'AC', "The pattern found does't match with expected result!"
+        assert result == 'TA', "The pattern found does't match with expected result!"
         print('The result is matching, Test is Passed!')
     elif run_mode in ['F', 'f']:
         result = main(full_path)
